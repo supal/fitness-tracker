510dba519b5a58beee064b6865219f375b5775f0
supal <supal.iut03@gmail.com>
2023-12-29 00:56:51 +0600

    datatable added

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -14,6 +14,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatSelectModule } from '@angular/material/select';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
 import {
   MatDialog,
   MAT_DIALOG_DATA,
@@ -44,6 +45,7 @@ import {
     MatDialogClose,
     MatDialogContent,
     MatTableModule,
+    MatSortModule,
   ],
   exports: [
     MatButtonModule,
@@ -64,6 +66,7 @@ import {
     MatDialogClose,
     MatDialogContent,
     MatTableModule,
+    MatSortModule,
   ],
 })
 export class MaterilaModule {}
--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-past-training',
   templateUrl: './past-training.component.html',
   styleUrls: ['./past-training.component.css'],
 })
-export class PastTrainingComponent implements OnInit {
+export class PastTrainingComponent implements OnInit, AfterViewInit {
   displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
   dataSource = new MatTableDataSource<Exercise>();
 
+  @ViewChild(MatSort)
+  sort!: MatSort;
+
   constructor(private trainingService: TrainingService) {}
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+  }
 
   ngOnInit(): void {
     this.dataSource.data =
